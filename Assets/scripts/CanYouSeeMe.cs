cedb456c66caa5f1d4995d1b0a760a2582cb3ac0
redmartian-droid <118920978+redmartian-droid@users.noreply.github.com>
2023-08-19 23:08:23 +0200

    test 4

--- Assets/scripts/CanYouSeeMe.cs
+++ Assets/scripts/CanYouSeeMe.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CanYouSeeThis : MonoBehaviour
+public class CanYouSeeMe : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
